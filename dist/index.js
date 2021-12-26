410e8831d4c6b05e87a1d7b649a5fb9372b64271
augusto-dev <augusto_nog@outlook.es>
2021-12-26 15:29:14 -0300

    Revert "Revert "Revert "mejorada estructura user"""
    This reverts commit b385624f7bea2005a593d7a5d33fc20142581efa.

--- dist/index.js
+++ dist/index.js
@@ -43,7 +43,7 @@ require("reflect-metadata");
 var express_1 = __importDefault(require("express"));
 var morgan_1 = __importDefault(require("morgan"));
 var cors_1 = __importDefault(require("cors"));
-var routes_1 = __importDefault(require("./routes"));
+var user_routes_1 = __importDefault(require("./routes/user.routes"));
 var typeorm_1 = require("typeorm");
 var app = express_1.default();
 // al ejecutar busca el ormconfig y ya conoce los parametros de la db
@@ -58,6 +58,6 @@ app.use(cors_1.default());
 app.use(morgan_1.default('dev'));
 app.use(express_1.default.json());
 //routes
-app.use(routes_1.default);
+app.use(user_routes_1.default);
 app.listen(3000);
 console.log('Server on port', 3000);
--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,22 +1,42 @@
-import { Get, Route, Tags, Post, Body, Path } from "tsoa";
-import { User } from '../models/User'
-import { getUsers, createUser, IUserPayload, getUser } from '../repositories/user.repository'
+import { Request, Response } from "express";
+import { getRepository } from "typeorm";
+import { Category } from "../entity/Category";
+import { Product } from "../entity/Product";
+import { User } from "../entity/User";
 
-@Route("users")
-@Tags("User")
-export default class UserController {
-    @Get("/")
-    public async getUsers(): Promise<Array<User>> {
-        return getUsers()
-    }
+export const getUsers = async (req: Request, res: Response): Promise<Response> => {
+    const users = await getRepository(User).find();
+    return res.json(users);
+}
 
-    @Post("/")
-    public async createUser(@Body() body: IUserPayload): Promise<User> {
-        return createUser(body)
-    }
+export const getUser = async (req: Request, res: Response): Promise<Response> => {
+    const user = await getRepository(User).findOne(req.params.id);
+    return res.json(user);
+}
 
-    @Get("/:id")
-    public async getUser(@Path() id: string): Promise<User | null> {
-        return getUser(Number(id))
+
+export const updateUser = async (req: Request, res: Response): Promise<Response> => {
+    const user = await getRepository(User).findOne(req.params.id);
+    if (user) {
+        getRepository(User).merge(user, req.body);
+        const result = await getRepository(User).save(user);
+        return res.json(result);
     }
+    return res.status(404).json({ error: 'User not found' });
+}
+
+
+export const createUser = async (req: Request, res: Response): Promise<Response> => {
+    try {
+        const newUser = getRepository(User).create(req.body);
+        return res.status(201).json(newUser);
+    } catch (err) {
+        console.log('err.message :: ', err);
+        return res.status(500).json({ message: 'Error internal server: ' });
+    }
+}
+
+export const deleteUser = async (req: Request, res: Response): Promise<Response> => {
+    const result = await getRepository(User).delete(req.params.id);
+    return res.json(result);
 }
